67e36a56b159bc975c7bdb9acf1e17bc83d60b20
hallmee <116854172+hallmee@users.noreply.github.com>
2022-11-07 15:50:19 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,6 +24,68 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], "/slack/ev
 web_client = WebClient(token=os.environ['BOT_TOKEN'])
 
 # Functions we'd implement would be here.
+def parsing(self):
+        #split the function, so that all the instances of s,m,h,d and any spaces are separated
+        #but still kept
+        splits = re.split(r"([s|m|h|d| ])",self)
+        #remove all of the empty spaces from the newly created array after the split to create
+        #the resulting array
+        result = [x for x in splits if x != '' and x != ' ']
+        return result
+
+def calculation(self):
+        #initialize accumulator
+        i = 0
+        #initialize result paramter
+        res = 0
+        #traverse through the array
+        while i < len(self):
+                if i + 1 < len(self):
+                        #if there is still at least one more spot in the array, then there
+                        #is a possibliity that the next spot in the array is one of the s,m,h,d
+                        #characters, so check to see if it is any o those characters
+                        if self[i + 1] == 's':
+                                res = res + int(self[i])
+                                 i = i + 2
+                        elif self[i + 1] == 'm':
+                                res = res + (int(self[i]) * 60)
+                                i = i + 2
+                        elif self[i + 1] == 'h':
+                                res = res + (int(self[i]) * 3600)
+                                i = i + 2
+                        elif self[i + 1] == 'd':
+                                res = res + (int(self[i]) * 86400)
+                                i = i + 2
+                        else:
+                                #if the next spot in the array is none of those characters, then
+                                #since all of the extra spaces have already been removed, then the
+                                #next part of the array is another random letter, so remove that letter
+                                #with split
+                                 tmp = self[i].split('[a-z]')
+                                #since no s,m,h,d was specificied, default to calculating seconds
+                                #per minute
+                                res = res + (int(tmp[0]) * 60)
+                                i = i + 1
+                else:
+                        tmp = re.split(r"([a-z])",self[i])
+                        res = res + (int(tmp[0]) * 60)
+                        i = i + 1
+
+        return res
+
+@bot_app.route('/meetup',methods = ['POST'])
+def meetup(self):
+        step1 = parsing(self)
+        step2 = calculation(step1)
+        return step2
+
+
+def wait_message(ts,channel,meetremind):
+        data = {channel : meetremind}
+        db.reminder[ts] = data
+def in_five(ts):
+        return db.reminder[ts] != NULL
+                                
 
 # Allows us to set up a webpage with the script, which enables testing using tools like ngrok.
 if __name__ == "__main__":
