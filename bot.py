b21bf1a7578667ab4fdfe685731427f52283378d
Nix <48928368+Nixolium@users.noreply.github.com>
2022-11-07 16:25:06 -0600

    Connecting commands
    - Formatting
    - Connecting wait messages
    - wait_time
    - in_five

--- bot.py
+++ bot.py
@@ -1,93 +1,111 @@
 from dotenv import load_dotenv
 import json
 import os
+import time
 from slack import WebClient
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
-# Load the tokens from the ".env" file, which are set up as environment variables. 
-# You'll need the signing secret and bot token from the Slack developer console 
-# for a workspace, which you put in a file you make called ".env". It prevents a 
-# security risk related to revealing secret keys in public. 
+# Load the tokens from the ".env" file, which are set up as environment variables.
+# You'll need the signing secret and bot token from the Slack developer console
+# for a workspace, which you put in a file you make called ".env". It prevents a
+# security risk related to revealing secret keys in public.
 load_dotenv()
 
 bot_app = Flask(__name__)
 
-# You can find the signing secret token in the "Basic Information" -> "App Credentials" sections 
+# You can find the signing secret token in the "Basic Information" -> "App Credentials" sections
 # of the Slack workspace developer console. By "/slack/events" is the endpoint that you would
 # attach to the end of the ngrok link when inputting the request URL in the "Event Subscriptions"
-# -> "Enable Events" section of the Slack workspace developer console.  
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], "/slack/events", bot_app)
+# -> "Enable Events" section of the Slack workspace developer console.
+slack_event_adapter = SlackEventAdapter(
+    os.environ['SIGNING_SECRET'], "/slack/events", bot_app)
 
-# You can find the bot token in the "OAuth & Permissions" section of the Slack workspace developer 
+# You can find the bot token in the "OAuth & Permissions" section of the Slack workspace developer
 # console.
 web_client = WebClient(token=os.environ['BOT_TOKEN'])
 
+# Global Storage
+# This is an inefficient way to store data, but it is a temporary solution until we are able to
+# implement fireBase into our project.
+db = {}
+db.reminder = {}
+
 # Functions we'd implement would be here.
+
+
 def parsing(self):
-        #split the function, so that all the instances of s,m,h,d and any spaces are separated
-        #but still kept
-        splits = re.split(r"([s|m|h|d| ])",self)
-        #remove all of the empty spaces from the newly created array after the split to create
-        #the resulting array
-        result = [x for x in splits if x != '' and x != ' ']
-        return result
+    # split the function, so that all the instances of s,m,h,d and any spaces are separated
+    # but still kept
+    splits = re.split(r"([s|m|h|d| ])", self)
+    # remove all of the empty spaces from the newly created array after the split to create
+    # the resulting array
+    result = [x for x in splits if x != '' and x != ' ']
+    return result
+
 
 def calculation(self):
-        #initialize accumulator
-        i = 0
-        #initialize result paramter
-        res = 0
-        #traverse through the array
-        while i < len(self):
-                if i + 1 < len(self):
-                        #if there is still at least one more spot in the array, then there
-                        #is a possibliity that the next spot in the array is one of the s,m,h,d
-                        #characters, so check to see if it is any o those characters
-                        if self[i + 1] == 's':
-                                res = res + int(self[i])
-                                 i = i + 2
-                        elif self[i + 1] == 'm':
-                                res = res + (int(self[i]) * 60)
-                                i = i + 2
-                        elif self[i + 1] == 'h':
-                                res = res + (int(self[i]) * 3600)
-                                i = i + 2
-                        elif self[i + 1] == 'd':
-                                res = res + (int(self[i]) * 86400)
-                                i = i + 2
-                        else:
-                                #if the next spot in the array is none of those characters, then
-                                #since all of the extra spaces have already been removed, then the
-                                #next part of the array is another random letter, so remove that letter
-                                #with split
-                                 tmp = self[i].split('[a-z]')
-                                #since no s,m,h,d was specificied, default to calculating seconds
-                                #per minute
-                                res = res + (int(tmp[0]) * 60)
-                                i = i + 1
-                else:
-                        tmp = re.split(r"([a-z])",self[i])
-                        res = res + (int(tmp[0]) * 60)
-                        i = i + 1
+    # initialize accumulator
+    i = 0
+    # initialize result paramter
+    res = 0
+    # traverse through the array
+    while i < len(self):
+        if i + 1 < len(self):
+            # if there is still at least one more spot in the array, then there
+            # is a possibliity that the next spot in the array is one of the s,m,h,d
+            # characters, so check to see if it is any o those characters
+            if self[i + 1] == 's':
+                res = res + int(self[i])
+                i = i + 2
+            elif self[i + 1] == 'm':
+                res = res + (int(self[i]) * 60)
+                i = i + 2
+            elif self[i + 1] == 'h':
+                res = res + (int(self[i]) * 3600)
+                i = i + 2
+            elif self[i + 1] == 'd':
+                res = res + (int(self[i]) * 86400)
+                i = i + 2
+            else:
+                # if the next spot in the array is none of those characters, then
+                # since all of the extra spaces have already been removed, then the
+                # next part of the array is another random letter, so remove that letter
+                # with split
+                tmp = self[i].split('[a-z]')
+                # since no s,m,h,d was specificied, default to calculating seconds
+                # per minute
+                res = res + (int(tmp[0]) * 60)
+                i = i + 1
+        else:
+            tmp = re.split(r"([a-z])", self[i])
+            res = res + (int(tmp[0]) * 60)
+            i = i + 1
+    return res
 
-        return res
 
-@bot_app.route('/meetup',methods = ['POST'])
+@bot_app.route('/meetup', methods=['POST'])
 def meetup(self):
-        step1 = parsing(self)
-        step2 = calculation(step1)
-        return step2
+    step1 = parsing(self.payload.text)
+    ts = calculation(step1)
+    wait_message(ts, self.payload.channel, "reminder for meetup")
+    return ts + time.time()
 
 
-def wait_message(ts,channel,meetremind):
-        data = {channel : meetremind}
-        db.reminder[ts] = data
+def wait_message(ts, channel, remindMessage):
+    if not db.reminder[ts]:
+        db.reminder[ts] = {}
+    db.reminder[ts].update({channel: remindMessage})
+    return
+
+
 def in_five(ts):
-        return db.reminder[ts] != NULL
-                                
+    for time in db.reminder:
+        if (time - time.time() < 300):
+            return True
+    return False
+
 
 # Allows us to set up a webpage with the script, which enables testing using tools like ngrok.
 if __name__ == "__main__":
     bot_app.run(debug=True)
-    
--- test.py
+++ test.py
@@ -1,69 +1,74 @@
 import unittest
 import time
-from firebase import Firebase
+#from firebase import Firebase
 
-#this first set of unit tests makes sure that the formatting of the time and calculating the value in seconds
-#works correctly. "s" after a numerical value represents seconds, "d" represents days, and "h" represents hours, and "m" represents. 
-#All other letters that are used will cause the number they are after to default to minutes. The omission of s,d,h,m, will cause the input value
-#to default to minutes. Arbitrary numbers of spaces should also be valid, and any order of the seconds, minutes, etc. should be valid.
+# this first set of unit tests makes sure that the formatting of the time and calculating the value in seconds
+# works correctly. "s" after a numerical value represents seconds, "d" represents days, and "h" represents hours, and "m" represents.
+# All other letters that are used will cause the number they are after to default to minutes. The omission of s,d,h,m, will cause the input value
+# to default to minutes. Arbitrary numbers of spaces should also be valid, and any order of the seconds, minutes, etc. should be valid.
+
+
 class TestMeetupMessage(unittest.TestCase):
-        def test_something(self):
-                #tests if seconds returns correct second value.
-                self.assertEqual(meetup("1s",1))
-                #tests if minutes returns correct second value.
-                self.assertEqual(meetup("60m"),3600)
-                #tests if days returns correct second value.
-                self.assertEqual(meetup("1d"),86400)
-                #tests if multiple units days = d, second = s, minute = m, can return successfully.
-                self.assertEqual(meetup("1d30m"),88200)
-                #tests whether extra space can return successfully.
-                self.assertEqual(meetup("1d 30m"),88200)
-                #tests if multipe ordering of the different time units can return successfully.
-                self.assertEqual(meetup("30m1d"),88200)
-                #tests if an unknown letter/unit is entered that return value defaults to number of seconds
-                #per minute
-                self.assertEqual(meetup("60q"),3600)
-                #tests if absence of letter/unit return value defaults to number of seconds per minute
-                self.assertEqual(meetup("60"),3600)
-                #tests whether multiple values, in the absence of a unit, are added together and then
-                #return value is converted to number of seconds per minute.
-                self.assertEqual(meetup("30 30"),3600000)
-                #check to see if the function works properly with the optional location parameter
-                self.assertEqual(meetup("60m", "Zoom"),3600)
+    def test_something(self):
+        # tests if seconds returns correct second value.
+        self.assertEqual(meetup("1s", 1))
+        # tests if minutes returns correct second value.
+        self.assertEqual(meetup("60m"), 3600)
+        # tests if days returns correct second value.
+        self.assertEqual(meetup("1d"), 86400)
+        # tests if multiple units days = d, second = s, minute = m, can return successfully.
+        self.assertEqual(meetup("1d30m"), 88200)
+        # tests whether extra space can return successfully.
+        self.assertEqual(meetup("1d 30m"), 88200)
+        # tests if multipe ordering of the different time units can return successfully.
+        self.assertEqual(meetup("30m1d"), 88200)
+        # tests if an unknown letter/unit is entered that return value defaults to number of seconds
+        # per minute
+        self.assertEqual(meetup("60q"), 3600)
+        # tests if absence of letter/unit return value defaults to number of seconds per minute
+        self.assertEqual(meetup("60"), 3600)
+        # tests whether multiple values, in the absence of a unit, are added together and then
+        # return value is converted to number of seconds per minute.
+        self.assertEqual(meetup("30 30"), 3600000)
+        # check to see if the function works properly with the optional location parameter
+        self.assertEqual(meetup("60m", "Zoom"), 3600)
 
-        def test_waitminute(self):
-                self.payload =
-                        {
-                                "type": "message",
-                                "channel":"C048LKTG8NS",
-                                "user": "U04995560FK",
-                                "text": "!meetup 5h",
-                                "ts": "13334344"
-                        }
-                #the "reminder" of db.reminder represents the contents of the database, which stores the timestamps
-                #since nothing is entered in the database, this first test should return false, because there are no
-                #objects within the database at all.
-                assertFalse(in_five(db.reminder)) 
-                #sets the timestamp of the payload to a value that is over five minutes from the current time.
-                self.payload.ts = time.time() + 1000
-                #enteres the d time value, the channel ID, and the message into the database.
-                wait_message(self.payload.ts,self.payload.channel,"reminder for meetup")
-                #since d is not within five minutes of the current time, there are no times within the database
-                #that are within the current time, so return false.
-                assertFalse(in_five(db.reminder))
-                #sets t to a value that is within five seconds of the current time.
-                self.payload.ts = time.time() + 5
-                #this enters a time within five minutes of the current time into database.
-                wait_message(self.payload.ts,self.payload.channel,"reminder for meetup")
-                #goes into database, retrieves time value stored at "reminder", and sees if it is successful.
-                assertEqual(db.reminder[self.payload.ts], {self.payload.channel: "reminder for meetup"})
-                #goes into database, retrieves time value stored at "reminder", and sees if it is successful.
-                assertEqual(db.reminder[self.payload.ts], {self.payload.channel: "reminder for meetup"})
-                #checks if there are any scheduled reminder messages in five minutes, returns true
-                #because t is within five minutes of current time.
-                assertTrue(in_five(db.reminder))
+    def test_waitminute(self):
+        self.payload = {
+            "type": "message",
+            "channel": "C048LKTG8NS",
+            "user": "U04995560FK",
+            "text": "!meetup 5h",
+            "ts": "13334344"
+        }
+        # the "reminder" of db.reminder represents the contents of the database, which stores the timestamps
+        # since nothing is entered in the database, this first test should return false, because there are no
+        # objects within the database at all.
+        assertFalse(in_five(db.reminder))
+        # sets the timestamp of the payload to a value that is over five minutes from the current time.
+        self.payload.ts = time.time() + 1000
+        # enteres the d time value, the channel ID, and the message into the database.
+        wait_message(self.payload.ts, self.payload.channel,
+                     "reminder for meetup")
+        # since d is not within five minutes of the current time, there are no times within the database
+        # that are within the current time, so return false.
+        assertFalse(in_five(db.reminder))
+        # sets t to a value that is within five seconds of the current time.
+        self.payload.ts = time.time() + 5
+        # this enters a time within five minutes of the current time into database.
+        wait_message(self.payload.ts, self.payload.channel,
+                     "reminder for meetup")
+        # goes into database, retrieves time value stored at "reminder", and sees if it is successful.
+        assertEqual(db.reminder[self.payload.ts], {
+                    self.payload.channel: "reminder for meetup"})
+        # goes into database, retrieves time value stored at "reminder", and sees if it is successful.
+        assertEqual(db.reminder[self.payload.ts], {
+                    self.payload.channel: "reminder for meetup"})
+        # checks if there are any scheduled reminder messages in five minutes, returns true
+        # because t is within five minutes of current time.
+        assertTrue(in_five(db.reminder))
 
 
 if __name__ == '__main__':
     unittest.main()
-"test.py" 71L, 3315C                                                                                     1,1           T
+"test.py" 71L, 3315C                                                                                     1, 1           T
