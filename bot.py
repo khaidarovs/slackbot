dce9d25109bfcc0577f048d73b178e2a961a73b7
= <=>
2022-11-08 20:01:18 -0600

    address merge conflict

--- bot.py
+++ bot.py
@@ -5,25 +5,25 @@ from slack import WebClient
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
-# Load the tokens from the '.env' file, which are set up as environment variables. 
-# You'll need the signing secret and bot token from the Slack developer console 
-# for a workspace, which you put in a file you make called '.env'. It prevents a 
+# Load the tokens from the ".env" file, which are set up as environment variables. 
+# You"ll need the signing secret and bot token from the Slack developer console 
+# for a workspace, which you put in a file you make called ".env". It prevents a 
 # security risk related to revealing secret keys in public. 
 load_dotenv()
 
 bot_app = Flask(__name__)
 
-# You can find the signing secret token in the 'Basic Information' -> 'App Credentials' sections 
-# of the Slack workspace developer console. By '/slack/events' is the endpoint that you would
-# attach to the end of the ngrok link when inputting the request URL in the 'Event Subscriptions'
-# -> 'Enable Events' section of the Slack workspace developer console.  
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', bot_app)
+# You can find the signing secret token in the "Basic Information" -> "App Credentials" sections 
+# of the Slack workspace developer console. By "/slack/events" is the endpoint that you would
+# attach to the end of the ngrok link when inputting the request URL in the "Event Subscriptions"
+# -> "Enable Events" section of the Slack workspace developer console.  
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], "/slack/events", bot_app)
 
-# You can find the bot token in the 'OAuth & Permissions' section of the Slack workspace developer 
+# You can find the bot token in the "OAuth & Permissions" section of the Slack workspace developer 
 # console.
 web_client = WebClient(token=os.environ['BOT_TOKEN'])
 
-# Functions we'd implement would be here.
+# Functions we"d implement would be here.
 
 # Allows us to set up a webpage with the script, which enables testing using tools like ngrok.
 if __name__ == "__main__":
