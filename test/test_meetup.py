64dd68caec13dc5f47be98ec067fb3a30086ce6e
hallmee <116854172+hallmee@users.noreply.github.com>
2022-11-14 17:34:07 -0500

    Update test_meetup.py

--- test/test_meetup.py
+++ test/test_meetup.py
@@ -7,24 +7,47 @@ from bot_meetup import *
 # works correctly. "s" after a numerical value represents seconds, "d" represents days, and "h" represents hours, and "m" represents.
 # All other letters that are used will cause the number they are after to default to minutes. The omission of s,d,h,m, will cause the input value
 # to default to minutes. Arbitrary numbers of spaces should also be valid, and any order of the seconds, minutes, etc. should be valid.
+# The unit tests that have #NEW as a comment a line before them are new unit tests for the second iteration. We want to make sure that when the meetup
+# function is called with a specific time specified, that it subsequently sends a message to everyone in the channel that there will be a meeting
+# in x amount of time, and that depending on what units (s,d,h,m) are used, how the message will be sent. Also, if an opitonal location parameter is 
+# added, then that should also be written in the message.
 
 class TestMeetupMessage(unittest.TestCase):
     def test_something(self):
+         self.payload = {
+                        "type": "message",
+                        "channel":"C048LKTG8NS",
+                        "user":"U04995560FK",
+                        "text":"/meetup 5h",
+                        "ts":"13334344"
+                }
         # tests if seconds returns correct second value.
         self.assertEqual(meetup("1s"), 1)
+        #NEW: tests if message is correctly sent in the payload.
+        self.assertEqual(self.payload["type"], "meetup in 1 second!")
         # tests if minutes returns correct second value.
         self.assertEqual(meetup("60m"), 3600)
+        #NEW: tests if message is correct sent in the payload.
+        self.assertEqual(self.payload["type"], "meetup in 60 minutes!")
         # tests if days returns correct second value.
         self.assertEqual(meetup("1d"), 86400)
+        #NEW: tests if message is correctly sent in the payload.
+        self.assertEqual(self.payload["type"], "meetup in 1 day!")
         # tests if multiple units days = d, second = s, minute = m, can return successfully.
         self.assertEqual(meetup("1d30m"), 88200)
+        #NEW: tests if message is correctly sent in the payload.
+        self.assertEqual(self.payload["type"], "meetup in 1 day and 30 minutes!")
         # tests whether extra space can return successfully.
         self.assertEqual(meetup("1d 30m"), 88200)
         # tests if multipe ordering of the different time units can return successfully.
         self.assertEqual(meetup("30m1d"), 88200)
+        #NEW: the order of the message should be the order that the time(s) were written in the command.
+        self.assertEqual(self.payload["type"], "meetup in 30 minutes and 1 day!")
         # tests if an unknown letter/unit is entered that return value defaults to number of seconds
         # per minute
         self.assertEqual(meetup("60q"), 3600)
+        #NEW: tests if message is correctly sent to the payload without s,m,h,d
+        self.assertEqual(self.payload["type"],"meetup in 30 minutes!")
         # tests if absence of letter/unit return value defaults to number of seconds per minute
         self.assertEqual(meetup("60"), 3600)
         # tests whether multiple values, in the absence of a unit, are added together and then
@@ -32,13 +55,15 @@ class TestMeetupMessage(unittest.TestCase):
         self.assertEqual(meetup("30 30"), 3600)
         # check to see if the function works properly with the optional location parameter
         #self.assertEqual(meetup("60m", "Zoom"), 3600)
+        #check to see that location is added to the message correctly.
+        self.assertEqual(self.payload["channel"],"meetup up at Zoom in 60 minutes!")
 
     def test_waitminute(self):
         self.payload = {
             "type": "message",
             "channel": "C048LKTG8NS",
             "user": "U04995560FK",
-            "text": "!meetup 5h",
+            "text": "/meetup 5h",
             "ts": "0"
         }
         # the "reminder" of db.reminder represents the contents of the database, which stores the timestamps
