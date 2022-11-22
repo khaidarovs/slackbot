86744e8f7fd72c34d818c595990a1446871ddccc
Mayurakshi Ghosal <mghosal@uchicago.edu>
2022-11-22 17:25:06 -0600

    added test case to test the summary model

--- test/test_convo_summary.py
+++ test/test_convo_summary.py
@@ -32,7 +32,7 @@ class Test_Slash_Command_Activity_Warnings(unittest.TestCase):
     # Tests the functionality of the conversation summary feature, which posts
     # a message in chat summarizing the messages in the past N hours (specified
     # by user doing the slash cmd). This tests if there are some msgs in the 
-    # past N hrs
+    # past N hrs, and also checks if the parse_messages
     def test_conv_summary_some_msgs(self):
         input = {
         "token":"test_token_1",
@@ -47,11 +47,19 @@ class Test_Slash_Command_Activity_Warnings(unittest.TestCase):
             {"I wanted to talk also review the interpretor pattern."},
             {"Ok cool. The interpretor pattern is also confusing for me"}
         ],
-        "hrs":5
+        "hrs": 5
         }
         self.payload = input
         n_msgs, msg_sent = summarize_conversation(self)
         self.assertEqual(n_msgs, len(input.get('dummy_messages')))
         self.assertTrue(msg_sent)
+
+    #Tests the functionality of the summarizer_model(), to see whether it is sending
+    #the right message to the chat
+    def test_summary_model(self):
+        input_msg = "I missed class today. Can somebody explain the observer pattern please? Sure, I can explain. It may be easier if we discuss this over the phone OK. Let's meet up at 7pm today I can't make 7pm, can we do later? Sure, let's meet at 9pm tonight Sounds good, do we want to discuss anything else? I wanted to talk also review the interpretor pattern. Ok cool. The interpretor pattern is also confusing for me"
+        summary = summary_model(input_msg)
+        output_msg = "It may be easier if we discuss this over the phone OK. Let's meet up at 7pm today I can't make 7pm, can we do later? Sure, let's meet at 9pm tonight Sounds good, do we want to discuss anything else?"
+        self.assertEqual(summary, output_msg)
 
         
